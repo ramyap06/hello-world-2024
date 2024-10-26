e765d0a524921910c9ef01f1e9a6f4b168ba1468
Sri Madur <sri.madur@gmail.com>
2024-10-26 14:09:07 -0400

    basic structure

--- src/App.js
+++ src/App.js
@@ -5,9 +5,22 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        {/* start writing */}
+        <div className="Title"><h1>title</h1></div>
+        <div className="Search-bar">search bar</div>
+        <div className="Title"><h3>what's your mood?</h3></div>
+        <div className="Album-sections">
+          <div className="Album"></div>
+          <div className="Album"></div>
+          <div className="Album"></div>
+        </div>
+
+
+        
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          hwrherfjerfj
         </p>
         <a
           className="App-link"
@@ -17,6 +30,7 @@ function App() {
         >
           Learn React
         </a>
+        {/* end of writing */}
       </header>
     </div>
   );
