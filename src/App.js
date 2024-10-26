8e9da1d764822ac4a6ce5e50eeadd1bd7a9c0abe
Sri Madur <sri.madur@gmail.com>
2024-10-26 16:46:05 -0400

    search bar progress

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useRef } from 'react';
 
 function App() {
+  const [mood, setMood] = useState('');
+  const inputRef = useRef(null);
+
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      setMood(inputRef.current.value);
+      inputRef.current.value = '';
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         {/* start writing */}
-        <div className="Title"><h1>title</h1></div>
-        <div className="Search-bar">search bar</div>
-        <div className="Mood-title"><h3>what's your mood?</h3></div>
+        <div className="Title"><h1>this is the mood app song generator that generates songs based off your mood i love you!!</h1></div>
+        <div className="Mood-title"><h3>What's your mood?</h3></div>
+        <div className="Search-bar">
+          <input
+            type = "text"
+            placeholder = "Enter your mood..."
+            ref={inputRef}
+            onKeyPress={handleKeyPress}
+          />
+        </div>
+
         <div className="Album-sections">
           <div className="Album">
             <img className="Album-cover" src="/logo192.png"></img>
