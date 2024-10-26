a8059d315319d9126c513db7a55c128a9c28300a
Budugu <budugu@pal-nat186-11-71.itap.purdue.edu>
2024-10-26 14:53:40 -0400

    flex

--- src/App.js
+++ src/App.js
@@ -10,26 +10,16 @@ function App() {
         <div className="Search-bar">search bar</div>
         <div className="Mood-title"><h3>what's your mood?</h3></div>
         <div className="Album-sections">
-          <div className="Album"></div>
-          <div className="Album"></div>
-          <div className="Album"></div>
+          <div className="Album">1</div>
+          <div className="Album">2</div>
+          <div className="Album">3</div>
         </div>
 
 
 
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          hwrherfjerfj
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+
+        
         {/* end of writing */}
       </header>
     </div>
